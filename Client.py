0451b28069634c813d410c20f8c83dc2946d4944
db368 <db368@njit.edu>
2019-04-18 12:59:34 -0400

    Add Secure SSL Functions to Server and Client

--- Client.py
+++ Client.py
@@ -1,5 +1,6 @@
 import socket
 import time
+import ssl
 
 port = 12000
 host_ip = '127.0.0.1'
@@ -26,8 +27,43 @@ def startConnection(host_ip, port):
     
     return sock
 
-s = startConnection(host_ip, port)
+def handle(conn):
+    conn.write(b'GET / HTTP/1.1\n')
+    print(conn.recv().decode())
 
-s.send("Ping".encode("utf-8"))
-response = s.recv(1024).decode("utf-8")
-print(response)
+def startSecureConnection(host_ip, port, cipher_list):
+    """Initiates a Secure Connection with the Server Module """
+    
+    # Create Socket and init request
+    initial_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock = ssl.wrap_socket(initial_sock, ssl_version = ssl.PROTOCOL_TLSv1, ciphers = cipher_list)
+    attempts = 0
+
+    # Do a quick try catch for the server not being up
+    while (attempts<3):
+        try: 
+            sock.connect((host_ip, port))
+            break
+        except ConnectionRefusedError:
+            if attempts == 2:
+                input("Connection refused. Terminating")
+                exit(0)
+            else:
+                print("Connection refused, retrying...")
+            time.sleep(1)
+        attempts = attempts + 1
+    
+    return sock
+
+# Initiate our connection
+s = startSecureConnection(host_ip, port, "IDEA-CBC-SHA")
+
+handle(s)
+#Send customary ping
+#s.send("Ping".encode("utf-8"))
+
+# Recieve and print pong
+#response = s.recv(1024).decode("utf-8")
+#print(response)
+
+
--- Server.py
+++ Server.py
@@ -1,10 +1,11 @@
 import socket
+import ssl
 
+ip = '127.0.0.1'
+port = 12000
 
-def initServer():
-    ip = '127.0.0.1'
-    port = 12000
-
+def initServer(ip, port):
+    """Starts an insecure server"""
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind((ip, port))
     sock.listen(1)
@@ -24,6 +25,41 @@ def initServer():
             continue
 
 
+def handle(conn):
+  print(conn.recv())
+  conn.write(b'HTTP/1.1 200 OK\n\n%s' % conn.getpeername()[0].encode())
+
+def initSecureServer(ip, port):
+    """Starts a secure server using SSL """
+
+    #Initiate a secure connection
+    init_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    init_sock.bind((ip, port))
+    init_sock.listen(1)
+    
+    print("Starting server")
+
+    # Listen loop
+    while(True):
+        try:
+            # Accept Incoming Connections
+            info = {}
+            connection, address = init_sock.accept()
+    
+            print("Connection get got")
+            # Wrap this connection in SSL
+            secure_sock = ssl.wrap_socket(connection, 
+            ssl_version=ssl.PROTOCOL_TLSv1, ciphers="IDEA-CBC-SHA", server_side = True, certfile = "server.crt", keyfile="privkey.pem", do_handshake_on_connect=True)
+            handle(secure_sock)
+
+           # print("Secure Connection Constructed")
+            #data = connection.recv(1024).decode("utf-8")
+            #print(data)
+            #response = "Pong"
+            #connection.send(response.encode("utf-8"))
+        except:
+            continue
 
 # Boot the server 
-initServer()
+initSecureServer(ip, port)
