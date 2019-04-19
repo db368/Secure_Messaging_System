188ecee61d81b669bad748072d19dee61eb673dc
db368 <db368@njit.edu>
2019-04-19 15:31:17 -0400

    Properly implement SSL

--- Client.py
+++ Client.py
@@ -3,7 +3,7 @@ import time
 import ssl
 
 port = 12000
-host_ip = '127.0.0.1'
+host_ip = 'localhost'
 
 def startConnection(host_ip, port):
     """Initiates a Connection with the Server Module """
@@ -28,15 +28,24 @@ def startConnection(host_ip, port):
     return sock
 
 def handle(conn):
+    """Send a request confirming our connection """
     conn.write(b'GET / HTTP/1.1\n')
     print(conn.recv().decode())
 
-def startSecureConnection(host_ip, port, cipher_list):
+def startSecureConnection(host_ip, port, cipher_list, cafile):
     """Initiates a Secure Connection with the Server Module """
     
-    # Create Socket and init request
-    initial_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock = ssl.wrap_socket(initial_sock, ssl_version = ssl.PROTOCOL_TLSv1, ciphers = cipher_list)
+    # Create Socket and context for server authentication
+    initial_sock = socket.socket(socket.AF_INET)
+    context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH, cafile=cafile)
+    # context.options |= ssl.HAS_TLSv1
+    # context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1 
+    # optional    # context.load_verify_locations("server.pem")
+    # sock = ssl.wrap_socket(initial_sock, ssl_version = ssl.PROTOCOL_TLSv1, ciphers = cipher_list)
+   # context.load_cert_chain(keyfile="server.key", certfile="server.pem", password="Miku") 
+    context.load_cert_chain(certfile="client.crt", keyfile = "client.key")
+    context.set_ciphers("HIGH:!aNULL:!kRSA:!PSK:!SRP:!MD5:!RC4")
+    sock = context.wrap_socket(initial_sock, server_hostname=host_ip)
     attempts = 0
 
     # Do a quick try catch for the server not being up
@@ -44,6 +53,8 @@ def startSecureConnection(host_ip, port, cipher_list):
         try: 
             sock.connect((host_ip, port))
             break
+
+        # If it doesn't work, try again a couple times
         except ConnectionRefusedError:
             if attempts == 2:
                 input("Connection refused. Terminating")
@@ -51,19 +62,21 @@ def startSecureConnection(host_ip, port, cipher_list):
             else:
                 print("Connection refused, retrying...")
             time.sleep(1)
+        # Check to see if certs work out
+        except ssl.SSLCertVerificationError as e:
+            print("Cert Refused!", e)
+            exit(-1)
+            break
         attempts = attempts + 1
-    
+        continue
+        # Check for ssl exceptions
+
     return sock
 
 # Initiate our connection
-s = startSecureConnection(host_ip, port, "IDEA-CBC-SHA")
+s = startSecureConnection(host_ip, port, "IDEA-CBC-SHA", "rootCA.pem")
 
 handle(s)
-#Send customary ping
-#s.send("Ping".encode("utf-8"))
 
-# Recieve and print pong
-#response = s.recv(1024).decode("utf-8")
-#print(response)
 
 
--- Server.py
+++ Server.py
@@ -26,40 +26,54 @@ def initServer(ip, port):
 
 
 def handle(conn):
-  print(conn.recv())
-  conn.write(b'HTTP/1.1 200 OK\n\n%s' % conn.getpeername()[0].encode())
+    """ Sends the client an ok message confirming successful HTTP connection"""
+    print(conn.recv())
+    conn.write(b'HTTP/1.1 200 OK\n\n%s' % conn.getpeername()[0].encode())
 
-def initSecureServer(ip, port):
+def initSecureServer(ip, port, cafile):
     """Starts a secure server using SSL """
+    print("Starting server")
 
     #Initiate a secure connection
     init_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     init_sock.bind((ip, port))
     init_sock.listen(1)
     
-    print("Starting server")
+    print("I'm all ears")
 
     # Listen loop
     while(True):
         try:
             # Accept Incoming Connections
             info = {}
+            secure_sock = None
             connection, address = init_sock.accept()
     
             print("Connection get got")
+            
             # Wrap this connection in SSL
-            secure_sock = ssl.wrap_socket(connection, 
-            ssl_version=ssl.PROTOCOL_TLSv1, ciphers="IDEA-CBC-SHA", server_side = True, certfile = "server.crt", keyfile="privkey.pem", do_handshake_on_connect=True)
+            # secure_sock = ssl.wrap_socket(connection, 
+            #ssl_version=ssl.PROTOCOL_TLSv1, ciphers="IDEA-CBC-SHA", server_side = True, certfile = "server.crt", keyfile="privkey.pem", do_handshake_on_connect=True)
+            
+            # Define context and wrap socket
+            context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH, cafile = cafile)
+            #context.set_ciphers('TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_128_GCM_SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:DHE-RSA-AES256-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES256-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES128-SHA:DHE-RSA-AES128-SHA:AES256-GCM-SHA384:AES128-GCM-SHA256:AES256-SHA256:AES128-SHA256:AES256-SHA:AES128-SHA')
+            context.load_cert_chain(certfile="server.crt", keyfile = "server.key")
+            print("Cert Chain Locked and Loaded")
+            secure_sock = context.wrap_socket(connection, server_side = True)
             handle(secure_sock)
 
-           # print("Secure Connection Constructed")
+            print("Secure Connection Constructed")
             #data = connection.recv(1024).decode("utf-8")
             #print(data)
             #response = "Pong"
             #connection.send(response.encode("utf-8"))
+        except ssl.SSLError as e:
+            print("SSL ERROR", e)
+            continue
         except:
+            print("Excepted")
             continue
 
 # Boot the server 
-initSecureServer(ip, port)
+initSecureServer(ip, port, "rootCA.pem")
